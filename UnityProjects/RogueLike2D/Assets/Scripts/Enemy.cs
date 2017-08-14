3ee10c2c86b2ce68d2125675e4f1417bb1067fe4
Sergei <cjsboxex@gmail.com>
2017-08-14 12:43:53 +0600

    - All necessary prefabs, Level generation, scene transitions.

---
+++ UnityProjects/RogueLike2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject {
+	public int playerDamage;
+
+	private Animator animator;
+	private Transform target;
+	private bool skipMove;
+	// Use this for initialization
+	protected override void Start () 
+	{
+		GameManager.instance.AddEnemyToList(this);
+		animator = GetComponent<Animator>();
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+		base.Start();
+	}
+
+
+	protected override void AttemptMove<T>(int dirX, int dirY)
+	{
+		if (skipMove)
+		{
+			skipMove = false;
+			return;
+		}
+		base.AttemptMove<T>(dirX, dirY);
+		skipMove = true;
+	}
+
+	public void MoveEnemy()
+	{
+		int dirX = 0;
+		int dirY = 0;
+		if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+		{
+			dirY = target.position.y > transform.position.y ? 1 : -1;
+		}
+		else
+		{
+			dirX = target.position.x > transform.position.x ? 1 : -1;
+		}
+		AttemptMove<Player>(dirX, dirY);
+	}
+
+	protected override void OnCantMove<T> (T component)
+	{
+		Player hitPlayer = component as Player;
+
+		animator.SetTrigger("enemyAttack");
+
+		hitPlayer.LoseFood(playerDamage);
+	}
+}
---
+++ UnityProjects/RogueLike2D/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public AudioSource fxSource;
+	public AudioSource musicSource;
+	public static SoundManager instance = null;
+
+	public float lowPitchRange = 0.95f;
+	public float highPitchRange = 1.05f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
